4d18e2b7dcc740c4b195f61fdbb12e8c6d9d7567
Nasser Al-Ghaithi <nasser.al-ghaithi@jisr.net>
2023-04-19 10:52:07 +0300

    Template literals

---
+++ 4_template_literals/main.js
@@ -0,0 +1,9 @@
+let text = `Hello every one 
+I am front end developer`;
+console.log(text);
+
+let htmlElement = `<h1> Hello Every one </h1>
+                    <div>
+                    <p>lorem lorem lorem </p>
+                    </div>`;
+document.body.innerHTML = htmlElement;
