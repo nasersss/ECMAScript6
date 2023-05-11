b412c721a375538ee223b7e65c6463da1e42def8
Nasser Al-Ghaithi <nasser.al-ghaithi@jisr.net>
2023-05-11 10:33:35 +0300

    foreach

---
+++ 13_foreach/main.js
@@ -0,0 +1,26 @@
+let array = [10,20,30,40,56,60];
+
+// for (let i = 0; i < array.length; i++) {
+//      console.log (`index ${i} element ${array[i]}`);
+// }
+
+// array.forEach((element,index) => {
+//     console.log (`index ${index} element ${element}`);
+// });
+
+
+let set = new Set([1,1,2,2,3,3,4,4,5,5]);
+// set.forEach((element1, element2)  => {
+// console.log(element1,element2);
+// });
+
+let prices = new Map([
+    ['banana', 1],
+    ['orange', 2],
+    ['meat', 4]
+  ]);
+
+
+//   prices.forEach((k,v)=>{
+//     console.log(k,v);
+//   })
