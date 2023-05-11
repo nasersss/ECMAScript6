5cc772bc463156ba61ac5961668e7fcd2c8e628e
Nasser Al-Ghaithi <nasser.al-ghaithi@jisr.net>
2023-05-11 10:39:41 +0300

    weak set and weak map

---
+++ 14_weakset/main.js
@@ -0,0 +1,27 @@
+/**
+ *  Set
+ * ---Contain any type of values
+ * ---Get element count with size
+ * ---foreach available to iterate
+ * ---You can get its keys
+ * 
+ *  weak set
+ * ---Contain only type of values
+ * ---can't Get element count with size
+ * ---foreach is not available to iterate
+ * ---You can get its keys
+ */
+
+/**
+ *  Map
+ * ---Contain any type of values
+ * ---Get element count with size
+ * ---foreach available to iterate
+ * ---You can get its keys
+ * 
+ *  weak Map
+ * ---Contain only type of values
+ * ---can't Get element count with size
+ * ---foreach is not available to iterate
+ * ---You can get its keys
+ */
